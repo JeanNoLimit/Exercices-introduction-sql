4c2ee0c945b8b3a5726672310baa5e93680231e8
Jean-Noel <jean.noel.richert@gmail.com>
2023-04-11 16:15:53 +0200

    E2 - nbre de pers./lieu

--- resultats.sql
+++ resultats.sql
@@ -3,3 +3,12 @@ SELECT nom_lieu
 FROM lieu 
 WHERE nom_lieu 
 LIKE '%um';
+
+-- exercice 2 Nombre de personnages par lieu (trié par nombre de personnages décroissant).
+-- On peut rajouter des alias 
+SELECT nom_lieu,COUNT(p.id_lieu) AS nbGaulois
+FROM personnage p
+INNER JOIN lieu l ON p.id_lieu = l.id_lieu
+GROUP BY l.id_lieu
+ORDER BY nbGaulois DESC;
+
