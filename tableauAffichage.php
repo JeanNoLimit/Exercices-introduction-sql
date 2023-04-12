108544b977ea39ba7177eb4b189d500639322554
Jean-Noel <jean.noel.richert@gmail.com>
2023-04-12 11:45:17 +0200

    requete sql + affiche tableau (sans mise en forme)

--- tableauAffichage.php
+++ tableauAffichage.php
@@ -16,6 +16,39 @@ $db = new PDO(
 catch (Exception $e){
     die('Erreur : ' . $e->getMessage());
 }
+//requette sql pour récupérer nom des personnages, leur lieu d'habitation et la spécialité des perso. Trié par nom de lieu et nom perso
+$mysqlQuery='SELECT nom_personnage, nom_lieu,nom_specialite 
+            FROM personnage p
+            INNER JOIN lieu l ON p.id_lieu = l.id_lieu
+            INNER JOIN specialite s ON p.id_specialite = s.id_specialite
+            ORDER BY nom_lieu, nom_personnage;';
+//PersonnagesStatement contient le résultat de la requête sous la fomre d'un objet PDOStatement(https://www.php.net/manual/fr/class.pdostatement.php)
+$personnagesStatement = $db->prepare($mysqlQuery);
+//fetchAll va chercher les données dans personnagesStatement et permet de récupérer les données dans un format exploitable sous forme de tableau PHP
+$personnagesStatement->execute();
+$listePersonnages = $personnagesStatement->fetchAll();
+
+echo "<table>",
+        "<thread>",
+            "<tr>",
+                "<td>Nom du personnage</td>",
+                "<td>Lieu d'habitation</td>",
+                "<td>Spécialité </td>",
+            "</tr>",
+        "<thread>",
+        "<tbody>";
+
+
+foreach ($listePersonnages as $personnage) {
+    echo "<tr>",
+            "<td>".$personnage['nom_personnage']."</td>",
+            "<td>".$personnage['nom_lieu']."</td>",
+            "<td>".$personnage['nom_specialite']."</td>",
+        "</tr>";
+}
+
+echo    "</tbody>",
+    "</table>";
 
 
 ?>
